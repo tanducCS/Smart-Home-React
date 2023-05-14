3c6f7d1cf50e4a0b6ad4d407cef7e677b652344e
kieukhanhquan <kieukhanhquan@gmail.com>
2023-05-14 14:17:37 +0700

    update simple ai

--- Mobile/src/Screens/RoutineScreen/RoutineScreen.js
+++ Mobile/src/Screens/RoutineScreen/RoutineScreen.js
@@ -1,11 +1,62 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-const Tab = createMaterialBottomTabNavigator();
+import { View, Button } from 'react-native';
+import { Audio } from 'expo-av';
 
-const HomeScreen= () => {
+export default function RoutineScreen() {
+  const URL = "https://teachablemachine.withgoogle.com/models/t16iW2gKw/";
+
+    async function createModel() {
+        const checkpointURL = URL + "model.json"; // model topology
+        const metadataURL = URL + "metadata.json"; // model metadata
+
+        const recognizer = speechCommands.create(
+            "BROWSER_FFT", // fourier transform type, not useful to change
+            undefined, // speech commands vocabulary feature, not useful for your models
+            checkpointURL,
+            metadataURL);
+
+        // check that model and metadata are loaded via HTTPS requests.
+        await recognizer.ensureModelLoaded();
+
+        return recognizer;
+    }
+
+    async function init() {
+        const recognizer = await createModel();
+        const classLabels = recognizer.wordLabels(); // get class labels
+        const labelContainer = document.getElementById("label-container");
+        for (let i = 0; i < classLabels.length; i++) {
+            labelContainer.appendChild(document.createElement("div"));
+        }
+
+        // listen() takes two arguments:
+        // 1. A callback function that is invoked anytime a word is recognized.
+        // 2. A configuration object with adjustable fields
+        recognizer.listen(result => {
+            const scores = result.scores; // probability of prediction for each class
+            // render the probability scores per class
+            for (let i = 0; i < classLabels.length; i++) {
+                const classPrediction = classLabels[i] + ": " + result.scores[i].toFixed(2);
+                labelContainer.childNodes[i].innerHTML = classPrediction;
+            }
+        }, {
+            includeSpectrogram: true, // in case listen should return result.spectrogram
+            probabilityThreshold: 0.75,
+            invokeCallbackOnNoiseAndUnknown: true,
+            overlapFactor: 0.50 // probably want between 0.5 and 0.75. More info in README
+        });
+
+        // Stop the recognition in 5 seconds.
+        // setTimeout(() => recognizer.stopListening(), 5000);
+    }
   return (
-    <Text>Routine</Text>
+    <View marginTop="20%"
+    alignItems="center">
+      <Button
+        marginTop="100px"
+        title={'Start Recording'}
+        onClick={init()}
+      />
+    </View>
   );
 }
-export default HomeScreen;
--- Server/index.js
+++ Server/index.js
@@ -73,3 +73,4 @@ app.use('/humidity',humidityRouter);
 
 
 
+
